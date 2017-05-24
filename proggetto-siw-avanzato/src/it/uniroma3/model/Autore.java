7642a4dbc4be8d46ad6ee992f78c7dfb0eb7126f
enzo corsiero <enzocorsiero@172.20.10.3>
2017-05-24 16:55:53 +0200

    aggiunto persistence (da provare), aggiunte classi per operazioni crud ed aggiunta classe AutoreService

--- proggetto-siw-avanzato/src/it/uniroma3/model/Autore.java
+++ proggetto-siw-avanzato/src/it/uniroma3/model/Autore.java
@@ -1,6 +1,42 @@
 package it.uniroma3.model;
+import java.util.Date;
 
+import javax.persistence.*;
+
+@Entity
 public class Autore {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private Date dataNascita;
+	
+	public Autore(){
+		
+	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Date getDataNascita() {
+		return dataNascita;
+	}
+	public void setDataNascita(Date dataNascita) {
+		this.dataNascita = dataNascita;
+	}
+	
+	
 	
 	
 	
